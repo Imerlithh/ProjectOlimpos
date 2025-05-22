3392bc1d4d9b7909e085c72fe03cfe7494d40af1
Imerlithh <senelt16@itu.edu.tr>
2025-05-22 16:33:13 +0300

    Huge Gorm Update!

--- cmd/run.go
+++ cmd/run.go
@@ -1,7 +1,17 @@
 package cmd
 
+import (
+	"ProjectOlimpos/config"
+	"ProjectOlimpos/db/operations"
+	"ProjectOlimpos/web"
+)
+
 func Run() {
-	if firstLoad() {
-		startApplication()
+	// Config yoksa /first-run'a yönlendirme middleware'i çalışır.
+	if config.IsReady() {
+		operations.InitDB()
 	}
+
+	r := web.SetupRouter()
+	r.Run(":8080")
 }
